05abafcd59113520fbba015fa5b2109e3e9e5d81
Steve Oldfield <steveold1611@gmail.com>
2018-04-25 09:04:27 -0600

    setup computer to choose random choices and determine who wins.

--- assets/script/game.js
+++ assets/script/game.js
@@ -1,13 +1,68 @@
-function play(playerChoice){
-  var compChoice = "rock";
-  if (playerChoice == "paper") {
-      return "Player Wins";
-  }
-  else if (playerChoice == "scissor"){
-      return "Computer wins";
-  }
-  else {
-      return "It's a draw";
+
+function getRandom(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
   }
 
+function play(playerChoice){
+  //var compChoice = "rock";
+//
+  var compChoice = (getRandom(1, 3));
+
+  alert (compChoice);
+  if ((playerChoice) == (compChoice)){
+      return alert("It's a draw");
+  } 
+  else if
+  (playerChoice == 1 && compChoice == 2){
+      return alert("Computer Wins!!!")
+  }
+  else if 
+  (playerChoice == 1 && compChoice == 3){
+return alert("Player Wins!!!")
+  }
+  else if
+  (playerChoice == 2 && compChoice == 1){
+      return alert("Player Wins!!!")
+  }
+  else if
+  (playerChoice == 2 && compChoice == 3){
+      return alert("Computer Wins!!!")
+  }
+  else if
+  (playerChoice == 3 && compChoice == 1){
+      return alert("Player Wins!!!")
+  }
+  else if
+  (playerChoice == 3 && compChoice == 2){
+      return alert("Computer Wins!!!!")
+  }
+  
+
+//   if (playerChoice == "paper") {
+//       return alert("Player Wins");
+//   }
+//   else if (playerChoice == "scissor"){
+//     return alert("Computer wins");
+
+//   }
+//   else {
+//       return alert("It's a draw");
+//   }
+
 }
+
+
+//rock = 1 : 2 beats 1 : 1 beats 3 : 3 beats 2
+//paper = 2
+//sci = 3
+
+// paper = 1:  1 beats 2 : 2 beats 3 : 3 beats 1
+// rock = 2:
+// sci = 3:
+
+// sci - 1 : 1 beats 2 : 2 beats 3 ; 3 beats 1
+// paper - 2: 
+// rock - 3: 
+
